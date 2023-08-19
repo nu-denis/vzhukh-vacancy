425eb9a9af07a1ec5b00477a63141486b82f658b
nu-denis <nu.denis15@gmail.com>
2023-08-19 14:15:33 +0300

    fix back for CORS

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from page import router
 
 
 if __name__ == "__main__":
     app = FastAPI()
+    origins = ["*"]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     app.include_router(router)
     uvicorn.run(app, host="0.0.0.0", port=8000)
