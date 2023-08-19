6b473d0285e2261bbb96e49ddc6e40d0948a56dd
nu-denis <nu.denis15@gmail.com>
2023-08-19 13:19:11 +0300

    fix quots

--- backend/page/ai_endpoints.py
+++ backend/page/ai_endpoints.py
@@ -19,50 +19,50 @@ def get_text_from_resp(resp) -> str:
 
 def ai_request(messages: list):
     return openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model='gpt-3.5-turbo',
         messages=messages
     )
 
 
-@router.get("/open_format")
+@router.get('/open_format')
 async def open_format(content_text: str = ''):
     if not content_text:
         return ''
 
     return get_text_from_resp(ai_request([{
-        "role": "user",
-        "content": content_text
+        'role': 'user',
+        'content': content_text
     }]))
 
 
-@router.post("/open_format")
+@router.post('/open_format')
 async def open_format(content_text: Optional[str] = Body(embed=True)):  # TODO: validate parameter
     if not content_text:
         return ''
 
     return get_text_from_resp(ai_request([{
-        "role": "user",
-        "content": content_text
+        'role': 'user',
+        'content': content_text
     }]))
 
 
-@router.get("/vacancy_name")
-async def open_format(description: str = ''):
-    prompt_vacancy = "Предложи название вакансии по описанию: {} по шаблону: (Название вакансии: ОТВЕТ)"
+@router.get('/vacancy_name')
+async def vacancy_name(description: str = ''):
+    prompt_vacancy = 'Предложи название вакансии по описанию: {} по шаблону: (Название вакансии: ОТВЕТ)'
 
     return get_text_from_resp(ai_request([{
-        "role": "user",
-        "content": prompt_vacancy.format(description)
+        'role': 'user',
+        'content': prompt_vacancy.format(description)
     }]))
 
 
-@router.get("/vacancy_text")
-async def open_format(vacancy_name: str = '', description: str = ''):
+@router.get('/vacancy_text')
+async def vacancy_text(vacancy_name: str = '', description: str = ''):
     vacancy_blocks = 'Описание вакансии, Обязанности, Требования, Условия'
-    prompt_vacancy_text = "Напиши текст для вакансии: {} в вейп-шоп, с учётом текста: {} в формате Markdown и оставь " \
-                          "только разделы: {}"
+    prompt_vacancy_text = 'Напиши текст для вакансии: {} в вейп-шоп, с учётом текста: {} в формате Markdown и оставь ' \
+                          'только разделы: {}'
 
     return get_text_from_resp(ai_request([{
-        "role": "user",
-        "content": prompt_vacancy_text.format(vacancy_name, description, vacancy_blocks)
+        'role': 'user',
+        'content': prompt_vacancy_text.format(vacancy_name, description, vacancy_blocks)
     }]))
